b1bc1546e3118f748dfcd4e466909e6bb90dd3f7
Prachiti15 <91412980+Prachiti15@users.noreply.github.com>
2022-09-20 00:20:27 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,25 +2,30 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        ArrayList<arrPair> arrL = new ArrayList<arrPair>(10);
-        arrL.add(new arrPair(1, 2));
-        arrL.add(new arrPair(3, 4));
-        arrL.add(new arrPair(81, 50));
-        arrL.add(new arrPair(78, 35));
-        arrL.add(new arrPair(11, 26));
-        arrL.add(new arrPair(3, 5));
-        arrL.add(new arrPair(17, 55));
-        arrL.add(new arrPair(91, 27));
-        arrL.add(new arrPair(15, 27));
-        arrL.add(new arrPair(87, 25));
+        arrPair arrL[] = new arrPair[10];
+        arrL[0] = (new arrPair(1, 2));
+        arrL[1] = (new arrPair(3, 4));
+        arrL[2] = (new arrPair(81, 50));
+        arrL[3] = (new arrPair(78, 35));
+        arrL[4] = (new arrPair(11, 26));
+        arrL[5] = (new arrPair(3, 5));
+        arrL[6] = (new arrPair(17, 55));
+        arrL[7] = (new arrPair(91, 27));
+        arrL[8] = (new arrPair(15, 27));
+        arrL[9] = (new arrPair(87, 25));
+
+        List<arrPair> arrlist = new ArrayList<arrPair>(10);
+        arrlist = Arrays.asList(arrL);
         System.out.println("SORTED ON 1st value ");
-        Collections.sort(arrL, new aComparator());
+        Collections.sort(arrlist, new aComparator());
+        arrL = arrlist.toArray(new arrPair[arrlist.size()]);
         for (arrPair i : arrL) {
             i.display();
         }
 
         System.out.println("\nSORTED ON 2nd value ");
-        Collections.sort(arrL, new bComparator());
+        Collections.sort(arrlist, new bComparator());
+        arrL = arrlist.toArray(new arrPair[arrlist.size()]);
         for (arrPair i : arrL) {
             i.display();
         }
